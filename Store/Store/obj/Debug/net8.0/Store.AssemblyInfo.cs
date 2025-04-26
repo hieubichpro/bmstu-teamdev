46ea3cf5bb84d0ebf7fc6a68225dfc4c0be2d4cb
Pham Minh Hieu <1466-xf21iu26@users.noreply.git.iu7.bmstu.ru>
2025-04-26 17:20:36 +0300

    #23 - добавил сервисы для товаров

--- Store/Store/obj/Debug/net8.0/Store.AssemblyInfo.cs
+++ Store/Store/obj/Debug/net8.0/Store.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Store")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c9aeb6f0683afaf2f7d143ca2f5a1a617708a673")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b086cd56c5f732d71487d93fbd32cb2bdc3f8b1c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Store")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Store")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
