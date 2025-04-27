0a0615e1d3faee72cd57a92266caa502ef1ecd7f
Pham Minh Hieu <1466-xf21iu26@users.noreply.git.iu7.bmstu.ru>
2025-04-27 15:47:23 +0300

    #27 done

---
+++ backend/Store/Interfaces/IItemCartRepository.cs
@@ -0,0 +1,22 @@
+﻿using Store.Models;
+
+namespace Store.Interfaces
+{
+    public interface IItemCartRepository
+    {
+        ItemCart GetItemCart(int id);
+
+        void AddItemCart(ItemCart itemCart);
+
+        void DelItemCart(ItemCart itemCart);
+
+        void UpdateItemCart(ItemCart itemCart);
+
+        bool IsExistItemCart(ItemCart itemCart);
+        public bool IsExistItemCartWithProduct(ItemCart itemCart);
+
+
+        List<ItemCart> GetAllItemCartByIdCart(int id_cart);
+        List<ItemCart> GetAllItemCart();
+    }
+}
