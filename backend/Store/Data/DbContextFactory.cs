72722a04762f09fc26f3746cfd7145973937ce67
Pham Minh Hieu <1466-xf21iu26@users.noreply.git.iu7.bmstu.ru>
2025-04-27 16:54:08 +0300

    add DA layer

---
+++ backend/Store/Data/DbContextFactory.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Store.Data
+{
+    public class DbContextFactory
+    {
+        private readonly IConfiguration _configuration;
+
+        public DbContextFactory(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public DataContext CreateDbContext()
+        {
+            return new DataContext(new DbContextOptionsBuilder<DataContext>()
+                .UseNpgsql(_configuration.GetConnectionString("Connection"))
+                .Options);
+        }
+
+    }
+}
