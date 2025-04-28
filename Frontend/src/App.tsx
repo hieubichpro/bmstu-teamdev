d32ec9407895b3241a775c2f2f4e3b2988085983
Tai Nguyen <1200-nv20iu71@users.noreply.git.iu7.bmstu.ru>
2025-04-28 17:16:30 +0300

    #32 Adding User Cart Page

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -3,6 +3,7 @@ import { UserInfo } from "./components/pages/UserInfo";
 import { AdminUser } from "./components/pages/AdminUser";
 import { AdminOrder } from "./components/pages/AdminOrder";
 import { AdminProduct } from "./components/pages/AdminProduct";
+import { UserCart } from "./components/pages/UserCart";
 import { UserProduct } from "./components/pages/UserProduct";
 
 
@@ -14,10 +15,11 @@ export default function App() {
         <h1 className="h-screen flex w-full items-center bg-[#F6ECE7] justify-center">
           <Routes>
             <Route path="/user/:id/info" element={<UserInfo />} />
+            <Route path="/user/:id/cart" element={<UserCart />} />
+            <Route path="/user/:id/products" element={<UserProduct />} />
             <Route path="/admin/users" element={<AdminUser />} />
             <Route path="/admin/orders" element={<AdminOrder />} />
             <Route path="/admin/products" element={<AdminProduct />} />
-            <Route path="/user/:id/products" element={<UserProduct />} />
           </Routes>
         </h1>
     </BrowserRouter>
