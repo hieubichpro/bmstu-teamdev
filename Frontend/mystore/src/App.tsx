1cb442795685ba9f101b31b4486978a49f706423
Tai Nguyen <1200-nv20iu71@users.noreply.git.iu7.bmstu.ru>
2025-04-28 16:10:26 +0300

    #35 Adding Admin Orders Page

--- Frontend/mystore/src/App.tsx
+++ Frontend/mystore/src/App.tsx
@@ -1,5 +1,6 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { AdminUser } from "./components/pages/AdminUser";
+import { AdminOrder } from "./components/pages/AdminOrder";
 
 export default function App() {
   console.log("hello world");
@@ -9,6 +10,7 @@ export default function App() {
         <h1 className="h-screen flex w-full items-center bg-[#F6ECE7] justify-center">
           <Routes>
             <Route path="/admin/users" element={<AdminUser />} />
+            <Route path="/admin/orders" element={<AdminOrder />} />
           </Routes>
         </h1>
     </BrowserRouter>
